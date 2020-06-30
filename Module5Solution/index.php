ba9a333e4228069142b6a1e096ba4724ddff0ba6
neeraj <neerajkumar719714@gmail.com>
2020-07-01 00:46:50 +0530

    Mod5Soln

---
+++ Module5Solution/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Neeraj Singh PHP</title>
+</head>
+
+<body>
+    <h1>Neeraj Singh PHP</h1>
+
+    <?php
+    echo "hash of my name is<br>";
+        print hash('sha256','Neeraj Singh');
+    ?>
+
+
+    <p>Ascii art:</p>
+    <pre> 
+    |\  |
+    | \ |
+    |  \|        
+    </pre>
+</body>
+
+</html>
